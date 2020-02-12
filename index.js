654759911a2a1683c814d79a832c597ad7465475
jessykalage <jessykalage@gmail.com>
2020-02-12 17:17:07 -0300

    create database connection and tables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const customExpress = require('./config/customExpress')
-
 const databaseConnection = require('./infrastructure/connection')
+const Tabelas = require('./infrastructure/tables')
 
 databaseConnection.connect((error) => {
     if(!error){
+        Tabelas.init(conexao)
         const app = customExpress()
         app.listen(3000, () => { console.log('Listen 3000.')})
     }
--- infrastructure/connection.js
+++ infrastructure/connection.js
@@ -1,9 +1,12 @@
-const connection = require('mysql')
+const {HOST, PORT, USER, PASSWORD, DATABASE} = require('../credentials')
+const mysql = require('mysql')
 
-connection.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'user',
-    password: '',
-    database: 'agenda'
+const connection = mysql.createConnection({
+    host: HOST,
+    port: PORT,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
 })
+
+module.exports = connection
